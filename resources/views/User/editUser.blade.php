30868c2f4b6544a9e89ee3d14ddd92a18f75e313
oscarhumbe <oscarhumbe15@gmail.com>
2021-09-15 12:52:21 +0300

    edituser updt

--- resources/views/User/editUser.blade.php
+++ resources/views/User/editUser.blade.php
@@ -30,6 +30,7 @@
 
     <div class="card card-body mx-auto" style="width: 60%; border-radius:10px;">
         <form>
+            @csrf
             <div class="form-group row">
               <label for="inputFirstName" class="col-sm-auto col-form-label mx-3 ">First Name</label>
               <div class="col-md-8 form-inputs">
@@ -43,21 +44,27 @@
               </div>
             </div>
             <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-auto col-form-label mx-4">  &ensp; &ensp;Email</label>
+                <label for="inputEmail3" class="col-sm-auto col-form-label mx-3">Email</label>
                 <div class="col-sm-8 form-inputs">
-                  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                  <input type="email" class="form-control offset-1" id="inputEmail3" placeholder="Email">
                 </div>
               </div>
               <div class="form-group row">
-                <label for="inputRank" class="col-sm-auto col-form-label mx-4">  &ensp; &ensp;Rank</label>
-                <div class="col-sm-8 form-inputs">
-                  <input type="text" class="form-control" id="inputRank" placeholder="Rank">
+                <label for="inputRank" class="col-sm-auto col-form-label mx-3">Rank</label>
+                <div class="col-sm-8 form-inputs">              
+                    <select class="form-control offset-1" id="inputRank">
+                        <option selected>Choose</option>
+                        <option value="1">DICT</option>
+                        <option value="2">PMU</option>
+                        <option value="3">DIFT</option>
+                        <option value="4">DUD</option>   
+                      </select>
                 </div>
               </div>
               <div class="form-group row">
-                <label for="inputDivision" class="col-sm-auto col-form-label mx-3">  &ensp; &ensp;Division</label>
-                <div class="col-sm-8 form-inputs">
-                  <input type="text" class="form-control" id="inputDivision" placeholder="Division">
+                <label for="inputDivision" class="col-sm-auto col-form-label mx-3">Division</label>
+                <div class="col-sm-8 form-inputs ">
+                  <input type="text" class="form-control mx-3" id="inputDivision" placeholder="Division">
                 </div>
               </div>
               <div class="form-group row">
@@ -72,8 +79,20 @@
                   <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
                 </div>
               </div>
+              <div class="form-group row">
+                <label for="inputRole" class="col-sm-auto col-form-label mx-3 ">Role</label>
+                <div class="col-sm-8 form-inputs">              
+                    <select class="form-control offset-1" id="inputRole">
+                        <option selected>Choose</option>
+                        <option value="1">DICT</option>
+                        <option value="2">PMU</option>
+                        <option value="3">DIFT</option>
+                        <option value="4">DUD</option>   
+                      </select>
+                </div>
+              </div>
               
-                <button type="button" class="btn btn-primary col-2 offset-5" style="margin-top: 30px; background-color:#013c5c; border-radius:20px;" >Save</button>
+                <button type="button" class="btn btn-primary col-2 offset-4" style="margin-top: 30px; background-color:#013c5c; border-radius:20px;" >Save</button>
              
              
         </form>
