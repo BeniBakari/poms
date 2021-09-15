816fcdb0f6e5a2c525cd24d4690123e725ebe63d
oscarhumbe <oscarhumbe15@gmail.com>
2021-09-15 11:26:04 +0300

    header updates

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,15 +1,15 @@
 <header>
 
-	<div class=" container-fluid card-header" style="background-color: #013C5C;">
+	<div class=" container-fluid card-body" style="background-color: #013C5C;">
 		<h2 style="color: #ffffff; font-family: 'Raleway', sans-serif;" >POM-PORALG</h2>
 		{{-- <nav class="navbar navbar-expand-lg navbar-light">
 		  <a class="navbar-brand" style="color: #ffffff; font-family: 'Raleway', sans-serif;" href="#">POM-PORALG</a>
 		</nav> --}}
 	  </div>
-	  <div class="row">
+	  <div>
 		<nav class="navbar navbar-expand-lg navbar-light">
-			<a href=""> <i class="fas fa-align-justify px-2"></i></a>
-		   <a href="" class="offset-11" ><i class="fas fa-sign-out-alt mr-2 " style=""><span style=" font-family: 'Raleway', sans-serif;">Logout</span> </i></a> 
+			<a href="" style="text-decoration: none; color:black;"> <i class="fas fa-align-justify"></i></a>
+		   <a href="" class="offset-11" style="text-decoration: none; color:black;" ><i class="fas fa-sign-out-alt">Logout</i></a> 
 		  </nav>
 		
 	  </div>
